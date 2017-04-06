459d8bc819cf939502acab7792b0508c7733f053
skisas <paulius.juozelskis@ktu.edu>
2017-04-06 10:01:45 +0300

    Add project files.

---
+++ BayesianSpamFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BayesianSpamFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
